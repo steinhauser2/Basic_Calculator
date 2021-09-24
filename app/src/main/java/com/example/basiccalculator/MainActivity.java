8f82eb32c99afc5d94b9845c798697d286975964
Corey Steinhauser <steinhauser2@wisc.edu>
2021-09-24 15:19:52 -0500

    Basic Calculator working

--- app/src/main/java/com/example/basiccalculator/MainActivity.java
+++ app/src/main/java/com/example/basiccalculator/MainActivity.java
@@ -22,11 +22,46 @@ public class MainActivity extends AppCompatActivity {
         int int2 = Integer.parseInt(editText2.getText().toString());
 
         int ans = int1 + int2;
+        String str = "" + ans;
+        showAnswer(str);
     }
 
-    public void showAnswer(int ans) {
+    public void handleMinusButton(View view){
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        int int1 = Integer.parseInt(editText1.getText().toString());
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        int int2 = Integer.parseInt(editText2.getText().toString());
+
+        int ans = int1 - int2;
+        String str = "" + ans;
+        showAnswer(str);
+    }
+
+    public void handleMultButton(View view){
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        int int1 = Integer.parseInt(editText1.getText().toString());
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        int int2 = Integer.parseInt(editText2.getText().toString());
+
+        int ans = int1 * int2;
+        String str = "" + ans;
+        showAnswer(str);
+    }
+
+    public void handleDivButton(View view){
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        int int1 = Integer.parseInt(editText1.getText().toString());
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        int int2 = Integer.parseInt(editText2.getText().toString());
+
+        int ans = int1 / int2;
+        String str = "" + ans;
+        showAnswer(str);
+    }
+
+    public void showAnswer(String s) {
         Intent intent = new Intent(this, MainActivity2.class);
-        intent.putExtra("message", ans);
+        intent.putExtra("message", s);
         startActivity(intent);
     }
 }
